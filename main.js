adf852b1080ead2cbe12366dc37eb3edb497e573
PinkWorld9 <72043702+PinkWorld9@users.noreply.github.com>
2021-01-19 14:48:49 -0500

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-function adduser(){
-    Player1Name = document.getElementById("Player1 Name").value;
-    Player2Name = document.getElementById("Player2 Name").value;
-    localStorage.setItem("Player1Name", Player1Name);
-    localStorage.setItem("Player1Name", Player1Name);
-    window.location.replace("GamePage.html")
-}
